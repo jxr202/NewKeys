88e0c9a4b5c926bf4198f12f19f07bc2c7ff445c
jxr202 <jxr202@foxmail.com>
2017-09-05 00:32:50 +0800

    二叉搜索树

--- NewKeys.js
+++ NewKeys.js
@@ -28,6 +28,105 @@ function IsPopOrder(pushV, popV) {
     return stack.length === 0;
 }
 
-function PrintFromTopToBottom(root) {
-    // write code here
+/**
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ * @param root
+ * @constructor
+ */
+/* function TreeNode(x) {
+ this.val = x;
+ this.left = null;
+ this.right = null;
+ } */
+function pushNode(array, node) {
+    if (node.left) {
+        array.push(node.left.val);
+    }
+    if (node.right) {
+        array.push(node.right.val);
+    }
+    if (node.left) {
+        pushNode(array, node.left);
+    }
+    if (node.right) {
+        pushNode(array, node.right);
+    }
 }
+function PrintFromTopToBottom(root) {
+    let array = [];
+    if (root) {
+        array.push(root.val);
+        pushNode(array, root);
+    }
+    return array;
+}
+
+function PrintFromTopToBottomBest(root) {
+    let stack = [];
+    let array = [];
+    if (root) {
+        stack.push(root);
+    }
+    while (stack.length) {
+        let node = stack.shift();
+        array.push(node.val);
+        if (node.left) {
+            stack.push(node.left);
+        }
+        if (node.right) {
+            stack.push(node.right);
+        }
+    }
+    return array;
+}
+
+function Node(val, left, right) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+function BST() {
+    this.root = null;
+    this.insert = insert;
+}
+
+function insert(data) {
+    let node = new Node(data, null, null);
+    if (!this.root) {
+        this.root = node;
+    } else {
+        let current = this.root;
+        let parent;
+        while (current) {
+            parent = current;
+            if (data < current.val) {
+                current = parent.left;
+                if (!current) {
+                    parent.left = node;
+                    break;
+                }
+
+            } else {
+                if (!parent.right) {
+                    parent.right = node;
+                    break;
+                }
+                current = parent.right;
+            }
+        }
+    }
+}
+let node = new BST();
+node.insert(23);
+node.insert(45);
+node.insert(16);
+node.insert(37);
+node.insert(3);
+node.insert(99);
+node.insert(22);
+
+let array = [];
+array.push(node);
+
+console.log("array: " + JSON.stringify(array));
